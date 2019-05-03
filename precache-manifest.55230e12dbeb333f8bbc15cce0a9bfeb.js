2e9bd5e2b5e6dd37257f1f5592d2de70f9aea7b4
Fred <fredtbx@comcast.net>
2019-05-03 09:10:36 -0400

    Updates

---
+++ precache-manifest.55230e12dbeb333f8bbc15cce0a9bfeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6f962802cf053325fb44b0f595f950",
+    "url": "/getflix/index.html"
+  },
+  {
+    "revision": "5e8e8a81ab0347a39e27",
+    "url": "/getflix/static/css/main.3d46babe.chunk.css"
+  },
+  {
+    "revision": "45583c2e880a8852eea6",
+    "url": "/getflix/static/js/2.a6aeab30.chunk.js"
+  },
+  {
+    "revision": "5e8e8a81ab0347a39e27",
+    "url": "/getflix/static/js/main.52e4ce0a.chunk.js"
+  },
+  {
+    "revision": "b4d66ab2dbac5a1d6f85",
+    "url": "/getflix/static/js/runtime~main.cbe477ce.js"
+  }
+]);
